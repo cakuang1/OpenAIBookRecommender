8fa6fb02687ab44cdac904b8f6ec7f4aa59dd528
cakuang1 <70300980+cakuang1@users.noreply.github.com>
2023-08-24 23:55:21 -0700

    first commit

---
+++ moviemanagement/src/test/java/micro/cary/moviemanagement/MoviemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.cary.moviemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
